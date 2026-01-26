006bca3fd7f65528da1bee9186393c14756e131d
Alexandra Sofronie <alexandrateodora.sofronie@gracia.lasalle.cat>
2026-01-26 08:04:58 +0100

    Implemetar mapa y NPC sabio

--- images.g.ts
+++ images.g.ts
@@ -1,4 +1,4 @@
-// Auto-generated code. Do not edit.
+// Código generado automáticamente. No editar.
 namespace myImages {
 
     helpers._registerFactory("image", function(name: string) {
@@ -21,6 +21,207 @@ f f f c 3 c c c c 3 c f f f
 . 4 4 f 6 6 6 6 6 6 f 4 4 . 
 . . . . f f f f f f . . . . 
 . . . . f f . . f f . . . . 
+`;
+            case "image4":
+            case "arbol":return img`
+................86..................
+...........6688867886...............
+...........8666877688868............
+............868777767768............
+.........688667777776688............
+........67767777777778666...........
+.........6776667767666868...........
+..........866667667677688...........
+.........8666666666667778...........
+........667766666666666676..........
+.......67766667666776667776.........
+......886667776676777666688.........
+.....67766777667767777666768........
+....6776666666777667776666776.......
+.....8667776667766676677776776......
+......8777666666667776777776688.....
+....6887766776677677777777776776....
+..8866666677767777777777766666778...
+.86666666777667767777766666776668...
+..88677666666777677677666667776668..
+..86776677666666666666667776666668..
+886666677766667666666776677766668...
+6668666676667766767767766677666668..
+88866666666777677677667666666776668.
+.86668866666766776776666667766666668
+.86688666666666776666667667776666688
+.668866666666666666666677666666688..
+..8866686666666666677667776666668...
+...866886666666666677667776666668...
+...86886668666666667666666666888....
+....88866886686666666666666668......
+......86886668666866668666868.......
+......88866688668866688866888.......
+........8888888688888ce868..........
+..............e88e88.ec.8...........
+...............eeee..e..............
+...............ceef.ce..............
+...............ceefcec..............
+...............feefce...............
+...............fceeec...............
+...............ffceec...............
+`;
+            case "image3":
+            case "miImagen":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . f f f f . . . . . . 
+. . . . f f f 2 2 f f f . . . . 
+. . . f f f 2 2 2 2 f f f . . . 
+. . f f f e e e e e e f f f . . 
+. . f e e 2 2 2 2 2 2 e f f . . 
+. f f e 2 f f f f f f 2 e f f . 
+. f f f f f e e e e f f f f f . 
+. . f e f b f 4 4 f b f e f . . 
+. . f e 4 1 f d d f 1 4 e f . . 
+. . e f e 4 d d d d 4 e f . . . 
+. . e 4 d d e 2 2 2 2 f e f . . 
+. . . e d d e 2 2 2 2 f 4 e . . 
+. . . . e e f 5 5 4 4 f . . . . 
+. . . . . f f f f f f f . . . . 
+. . . . . . . . . f f f . . . . 
+`;
+            case "image6":
+            case "bridge":return img`
+.eeee..............eeee.
+eeddee............eeddee
+edbbde............edbbde
+edbbdeeeeeeeeeeeeeedbbde
+feddeebbbbbbbbbbbbfeddee
+feeeefbbbbbbbbbbbbfeeeef
+feeeefeebbbbbbbbeefeeeef
+eeddeffeeeeeeeeeeffeddee
+edbbdeffeeeeeeeeffedbbde
+edbbdebbbbbbbbbbbbedbbde
+feddeebbbbbbbbbbbbfeddee
+feeeefeebbbbbbbbeefeeeef
+feeeeffeeeeeeeeeeffeeeef
+eeddeeffeeeeeeeeffeeddee
+edbbdebbbbbbbbbbbbedbbde
+edbbdebbbbbbbbbbbbedbbde
+feddeeeebbbbbbbbeefeddee
+feeeeffeeeeeeeeeeffeeeef
+feeeefffeeeeeeeefffeeeef
+eeddeebbbbbbbbbbbbeeddee
+edbbdebbbbbbbbbbbbedbbde
+edbbdeeebbbbbbbbeeedbbde
+feddeefeeeeeeeeeeffeddee
+feeeefffeeeeeeeefffeeeef
+feeeefbbbbbbbbbbbbfeeeef
+eeddeebbbbbbbbbbbbeeddee
+edbbdeeebbbbbbbbeeedbbde
+edbbdefeeeeeeeeeefedbbde
+feddeeffeeeeeeeefffeddee
+feeeefbbbbbbbbbbbbfeeeef
+feeeefbbbbbbbbbbbbfeeeef
+eeddeeeebbbbbbbbeeeeddee
+edbbdefeeeeeeeeeefedbbde
+edbbdeffeeeeeeeeffedbbde
+feddeebbbbbbbbbbbbfeddee
+feeeefbbbbbbbbbbbbfeeeef
+fbeeefeebbbbbbbbeefbeeef
+fbbffffeeeeeeeeeeffbbfff
+febbbefffffffffffffebbbe
+feefff666666666666feefff
+feeeef666666666666feeeef
+eeeeef............eeeeef
+.eeff..............eeff.
+`;
+            case "image2":
+            case "sabio":return img`
+. . . . . . . c c c . . . . . . 
+. . . . . . c b 5 c . . . . . . 
+. . . . c c c 5 5 c c c . . . . 
+. . c c b c 5 5 5 5 c c c c . . 
+. c b b 5 b 5 5 5 5 b 5 b b c . 
+. c b 5 5 b b 5 5 b b 5 5 b c . 
+. . f 5 5 5 b b b b 5 5 5 c . . 
+. . f f 5 5 5 5 5 5 5 5 f f . . 
+. . f f f b f e e f b f f f . . 
+. . f f f 1 f b b f 1 f f f . . 
+. . . f f b b b b b b f f . . . 
+. . . e e f e e e e f e e . . . 
+. . e b c b 5 b b 5 b f b e . . 
+. . e e f 5 5 5 5 5 5 f e e . . 
+. . . . c b 5 5 5 5 b c . . . . 
+. . . . . f f f f f f . . . . . 
+`;
+            case "image5":
+            case "miImagen0":return img`
+.....75....757....57..7.....77..
+..5.55555.577775..755.7..7757...
+..5.577.565747.766777766775e777.
+....775.77577667677676776767777.
+55.757.75676777777777767c77667..
+7e5454777777765567776766677677.7
+..77757777777566667676566777677.
+.5.7657577775777777667677766766.
+.7777674576767777676677677776767
+..77..77777577777777577757767c67
+..775755777755777777577777776777
+.55557755.7567777777677777566.66
+.557777ee7677767767767767667677.
+.577.77777777767767e67776767....
+.5577777.775777776776776676777.6
+5..777777.77777777776767767766..
+5.557...77777777776767767667.66.
+..555.7777.77.756767cc667677676.
+...7...77777.777676657767776776.
+.....77....7.5577e7665.7...666..
+.....57....77..666767..7.6......
+.....7..........4eeee.6.........
+................46ee6...........
+................4eeec...........
+................4eeec...........
+...............e4eece...........
+...............eeeece...........
+..............44eeecce..........
+............444eeeeccc..........
+.........eee44ee.ececccec.......
+.......eeee.4ee..ece.cccec......
+....eee....e......e...eee.ccee..
+`;
+            case "image7":
+            case "diario":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . e e e e e e e e . . . . . . 
+. e e 1 1 1 1 1 1 e . . . . . . 
+. f e e e e e e 1 e . . . . . . 
+. f e e e e e e 1 e . . . . . . 
+. f e e e e e e 1 e . . . . . . 
+. f e e a a e e 1 e . . . . . . 
+. f e 5 5 5 5 e 1 e . . . . . . 
+. f e 5 5 5 5 e 1 e . . . . . . 
+. f e 5 5 5 5 e 1 e . . . . . . 
+. f e e e e e e 1 e . . . . . . 
+. f e e e e e e 1 e . . . . . . 
+. e e e e e e e 1 e . . . . . . 
+. . e e e e e e e e . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image8":
+            case "herrero":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . f f f f . . . . . . 
+. . . . f f f 2 2 f f f . . . . 
+. . . f f f 2 2 2 2 f f f . . . 
+. . f f f e e e e e e f f f . . 
+. . f e e 2 2 2 2 2 2 e f f . . 
+. f f e 2 f f f f f f 2 e f f . 
+. f f f f f e e e e f f f f f . 
+. . f e f b f 4 4 f b f e f . . 
+. . f e 4 1 f d d f 1 4 e f . . 
+. . e f f f f d d d 4 e f . . . 
+. . f d d d d f 2 2 2 f e f . . 
+. . f b b b b f 2 2 2 f 4 e . . 
+. . f b b b b f 5 4 4 f . . . . 
+. . . f c c f f f f f f . . . . 
+. . . . f f . . . f f f . . . . 
 `;
         }
         return null;
@@ -252,4 +453,4 @@ f f f c 3 c c c c c c f . .
     })
 
 }
-// Auto-generated code. Do not edit.
+// Código generado automáticamente. No editar.
